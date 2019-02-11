9e446959feb0f20e55c20de3c8b11fd088821200
Kumar Gaurav <gauravcusat1999@gmail.com>
2019-02-11 09:42:43 +0530

    changed homepage to home

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def homepage(request):
+def home(request):
     return render(request, 'home.html')
 
 def count(request):
